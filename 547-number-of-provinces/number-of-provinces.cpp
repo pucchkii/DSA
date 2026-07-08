7a9314219851f141655eb90da691bfe7824926a5
pucchkii <147378380+pucchkii@users.noreply.github.com>
2026-07-08 14:16:25 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (5.14%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-int n;
-    void DFS(vector<vector<int>>& isConnected, int u , vector<bool>& visited){
+    void bfs(unordered_map<int,vector<int>> &adj, int u , vector<bool>& visited){
         visited[u]=true;
-        for(int v=0 ; v<n ; v++){
-            if(!visited[v] && isConnected[u][v]==1){
-                DFS(isConnected,v,visited);
+        queue<int> q;
+        q.push(u);
+
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            for(auto& v: adj[curr]){
+                if(!visited[v]){
+                    bfs(adj,v,visited);
+
+                }
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        n=isConnected.size();
-        vector<bool> visited(n,false);
-        int cnt=0;
+        // using bfs
+        int n=isConnected.size(), cnt=0;
         // adj list
-        /* unordered_map<int,vector<int>> adj;
+        unordered_map<int,vector<int>> adj;
         for(int i=0 ; i<n ; i++){
             for(int j=0 ; j<n ; j++){
                 if(isConnected[i][j]==1){
@@ -22,14 +28,15 @@ int n;
                     adj[j].push_back(i);
                 }
             }
-        }*/
-        //dfs
+        }
+        vector<bool> visited(n,false);
         for(int i=0 ; i<n ; i++){
             if(!visited[i]){
-                DFS(isConnected,i,visited);
+                bfs(adj,i,visited);
                 cnt++;
             }
         }
         return cnt;
+        
     }
 };

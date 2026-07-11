a3fdddaaa9b93a2f1bda70f86d531a38d2256dad
pucchkii <147378380+pucchkii@users.noreply.github.com>
2026-07-11 13:46:15 +0530

    Time: 88 ms (41.58%) | Memory: 44.3 MB (68.36%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        unordered_map<int,vector<pair<int,int>>> adj;
+        vector<int> result(n+1,INT_MAX);
+        vector<int> parent(n+1);
+
+        for(auto & time : times){
+            int u=time[0];
+            int v=time[1];
+            int d=time[2];
+            adj[u].push_back({v,d});
+        }
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        pq.push({k,0});
+        result[k]=0;
+
+        for(int i=1 ; i<=n ; i++){
+            parent[i]=i;
+        }
+        while(!pq.empty()){
+            int node=pq.top().first;
+            int dist=pq.top().second;
+            pq.pop();
+            for(auto &vec : adj[node]){
+                int adjNode=vec.first;
+                int d=vec.second;
+                if(d+dist<result[adjNode]){
+                    result[adjNode]=d+dist;
+                    parent[adjNode]=node;
+                    pq.push({adjNode,d+dist});
+                }
+            }
+        }
+        int maxi=INT_MIN;
+        for(int i=1 ; i<=n ; i++){
+            maxi=max(maxi,result[i]);
+        }
+        if(maxi==INT_MIN || maxi==2147483647){
+            return -1;
+        }else return maxi;
+        
+    }
+};

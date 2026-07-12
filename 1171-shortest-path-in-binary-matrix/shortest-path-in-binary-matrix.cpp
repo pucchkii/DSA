fb1085c81b481dfc4a8687938a00cfa6c933e5a8
pucchkii <147378380+pucchkii@users.noreply.github.com>
2026-07-12 16:00:10 +0530

    Time: 243 ms (5.53%) | Memory: 82.7 MB (5.00%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> directions{{1,1},{0,1},{1,0},{-1,0},{0,-1},{-1,-1},{1,-1},{-1,1}};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
+
         int n=grid.size();
         int m=grid[0].size();
 
@@ -10,32 +10,32 @@ public:
             return -1;
         }
         auto isSafe=[&](int x, int y){
-            return x>=0 && x<m && y>=0 && y<n;
+            return x>=0 && x<m && y>=0 && y<n ;
         };
 
         queue<pair<int,int>> q;
         q.push({0,0});
         grid[0][0]=1;
-
         int level=0;
 
         while(!q.empty()){
             int N=q.size();
+
             while(N--){
                 auto curr=q.front();
-                q.pop();
-
                 int x=curr.first;
                 int y=curr.second;
-
+                q.pop();
+                
                 if(x==m-1 && y==n-1){
                     return level+1;
                 }
-                for(auto dir : directions){
-                    int x_ = x+dir[0];
-                    int y_ = y+dir[1];
 
-                    if(isSafe(x_,y_) && grid[x_][y_]==0){
+                for(auto dir : directions){
+                    int x_ = x + dir[0];
+                    int y_ = y + dir[1];
+
+                    if(isSafe( x_ , y_ ) && grid[x_][y_]==0){
                         grid[x_][y_]=1;
                         q.push({x_,y_});
                     }
@@ -43,6 +43,8 @@ public:
             }
             level++;
         }
-    return -1;
+        return -1;
+
+        
     }
 };

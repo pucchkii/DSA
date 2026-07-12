83072b839701a505a9c0840893d28859cb68a3fd
pucchkii <147378380+pucchkii@users.noreply.github.com>
2026-07-12 17:10:01 +0530

    Time: 41 ms (18.65%) | Memory: 23.9 MB (77.15%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,50 +1,52 @@
 class Solution {
 public:
-    vector<vector<int>> directions{{1,1},{0,1},{1,0},{-1,0},{0,-1},{-1,-1},{1,-1},{-1,1}};
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
 
+    typedef pair<int,pair<int,int>> P;
+
+    vector<vector<int>> directions{{1,1},{0,1},{1,0},{-1,0},{0,-1},{-1,-1},{1,-1},{-1,1}};
+
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
 
         if(n==0 || m==0 || grid[0][0]!=0){
             return -1;
         }
+
         auto isSafe=[&](int x, int y){
-            return x>=0 && x<m && y>=0 && y<n ;
+            return x>=0 && x<n && y>=0 && y<m;
         };
 
-        queue<pair<int,int>> q;
-        q.push({0,0});
-        grid[0][0]=1;
-        int level=0;
+        priority_queue<P,vector<P> , greater<P>> q;
+        vector<vector<int>> result(n,vector<int>(m,INT_MAX));
+
+        q.push({0,{0,0}});
+        result[0][0]=0;
 
         while(!q.empty()){
-            int N=q.size();
+            int d=q.top().first;
+            pair<int,int> node = q.top().second;
+            int x=node.first;
+            int y=node.second;
+            q.pop();
 
-            while(N--){
-                auto curr=q.front();
-                int x=curr.first;
-                int y=curr.second;
-                q.pop();
-                
-                if(x==m-1 && y==n-1){
-                    return level+1;
-                }
+            for(auto & dir : directions){
+                int x_ = x + dir[0];
+                int y_ = y + dir[1];
+                int dist=1;
 
-                for(auto dir : directions){
-                    int x_ = x + dir[0];
-                    int y_ = y + dir[1];
-
-                    if(isSafe( x_ , y_ ) && grid[x_][y_]==0){
-                        grid[x_][y_]=1;
-                        q.push({x_,y_});
-                    }
+                if(isSafe(x_ , y_) && grid[x_][y_]==0 && d+dist<result[x_][y_]){
+                    result[x_][y_]=d+dist;
+                    q.push({d+dist,{x_,y_}});
                 }
             }
-            level++;
         }
-        return -1;
+        if(result[n-1][m-1]==INT_MAX){
+            return -1;
+        }else{
+            return result[n-1][m-1]+1;
+        }
 
-        
+
     }
 };

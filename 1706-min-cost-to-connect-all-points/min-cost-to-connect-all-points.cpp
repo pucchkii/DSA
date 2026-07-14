aaa2764291d8da0fdcd6f565fd94e7be0361bc73
pucchkii <147378380+pucchkii@users.noreply.github.com>
2026-07-14 19:50:28 +0530

    Time: 682 ms (25.53%) | Memory: 180.3 MB (20.55%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,55 +1,76 @@
 class Solution {
-public:
-    typedef pair<int,int> P;
-
-    int spanningTree(int V, vector<vector<P>>& adj) {
-        // code here
-        priority_queue<P,vector<P>, greater<P>> pq;
-        vector<bool> inMST(V,false);
-        pq.push({0,0});
-        int sum=0;
+public:    
+    vector<int> parent;
+    vector<int> rank;
+    
+    int find(int u){
+        if(parent[u]==u){
+            return u;
+        }
+        return parent[u]=find(parent[u]);
+    }
+    
+    void Union(int x, int y){
+        int x_parent=find(x);
+        int y_parent=find(y);
         
-        while(!pq.empty()){
-            auto p=pq.top();
-            pq.pop();
-            int wt=p.first;
-            int node=p.second;
+        if(x_parent==y_parent){
+            return ;
+        }
+        
+        if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent]=x_parent;
+        }else if(rank[y_parent]>rank[x_parent]){
+            parent[x_parent]=y_parent;
+        }else{
+            parent[x_parent]=y_parent;
+            rank[y_parent]++;
+        }
+    }
+    int kruskal(vector<vector<int>> & vec){
+        int sum=0;
+        for(auto& temp : vec){
+            int u=temp[0];
+            int v=temp[1];
+            int wt=temp[2];
             
-            if(inMST[node]==true) continue;
-            inMST[node]=true;
-            sum+=wt;
+            int u_parent=find(u);
+            int v_parent=find(v);
             
-            for(auto& tmp : adj[node]){
-                int neighbour=tmp.first;
-                int neighbour_wt=tmp.second;
-                if(inMST[neighbour]==false){
-                    pq.push({neighbour_wt,neighbour});
-                }
+            if(u_parent!=v_parent){
+                sum+=wt;
+                Union(u,v);
             }
         }
         return sum;
-        
     }
-
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n=points.size();
+        int V=points.size();
+        parent.resize(V);
+        rank.resize(V,0);
+        
+        for(int i=0 ; i<V ; i++){
+            parent[i]=i;
+        }
 
-        vector<vector<P>> adj(n);
+        vector<vector<int>> vec;
 
-        for(int i=0 ; i<n ; i++){
-            for(int j=i+1 ; j<n ; j++){
+        for(int i=0 ; i<V ; i++){
+            for(int j=i+1 ; j<V ; j++){
                 int x1=points[i][0];
-                int x2=points[j][0];
-
                 int y1=points[i][1];
+
+                int x2=points[j][0];
                 int y2=points[j][1];
 
                 int d=abs(x1-x2)+abs(y1-y2);
-                adj[i].push_back({j,d});
-                adj[j].push_back({i,d});
-
+                vec.push_back({i,j,d});
             }
         }
-        return spanningTree(n,adj);
+        auto comparator=[&](vector<int>& v1, vector<int>& v2){
+            return v1[2]<v2[2];
+        };
+        sort(vec.begin(),vec.end(),comparator);
+        return kruskal(vec);
     }
 };

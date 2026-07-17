1f21aea414d84a5d5105af4312a6d26db4b98799
pucchkii <147378380+pucchkii@users.noreply.github.com>
2026-07-17 19:02:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (59.04%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    int t[101];
-    
-    int solve(vector<int>& nums, int i, int n){ // here i is the starting index
-        if(i>n){
-            return 0;
-        }
-        if(t[i]!=-1){
-            return t[i];
-        }
-
-        int steal = nums[i] + solve(nums,i+2,n);
-        int skip = solve(nums,i+1,n);
-
-        return t[i] = max(steal,skip);
-
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
+        vector<int> t(n+1,0);
 
-        if(n==1) return nums[0];
+        if(n==1){
+            return nums[0];
+        }
+        t[0]=0;
+        t[1]=nums[0];
 
-        if(n==2) return max(nums[0],nums[1]);
+        // case 1:(take 1st house hence, skip the last house)
+        for(int i=1 ; i<=n-1 ; i++){
+            int skip=t[i-1];
+            int steal=nums[i-1]+(i-2>=0 ? t[i-2] : 0);
+            t[i]=max(skip,steal);
+        }
+        int result1=t[n-1];
 
-        //taking 0th index house
-        memset(t,-1,sizeof(t));
-        int takeOthIndexHouse=solve(nums,0,n-2);
+        // case 2:(take 2nd house hence, taking the last house)
+        t.clear();
+        t[0]=0;
+        t[1]=0;
+        for(int i=2 ; i<=n ; i++){
+            int skip=t[i-1];
+            int steal=nums[i-1]+(i-2>=0 ? t[i-2] : 0);
+            t[i]=max(skip,steal);
+        }
+        int result2=t[n];
 
-        // taking last index house
-        memset(t,-1,sizeof(t));
-        int takeNthIndexHouse=solve(nums,1,n-1);
+        return max(result1,result2);
 
-        return max(takeOthIndexHouse,takeNthIndexHouse);
-        
+
     }
 };

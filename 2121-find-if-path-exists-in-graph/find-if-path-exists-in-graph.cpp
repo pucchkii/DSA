b55ea4e13e8cc1e8cf8716c59d0052f530331d93
pucchkii <147378380+pucchkii@users.noreply.github.com>
2026-07-17 13:24:24 +0530

    Time: 23 ms (95.76%) | Memory: 209.1 MB (95.26%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x){
+        if(parent[x]==x){
+            return x;
+        }
+        return parent[x]=find(parent[x]);
+    }
+
+    void Union(int x, int y){
+        int x_parent=find(x);
+        int y_parent=find(y);
+
+        if(x_parent==y_parent){
+            return;
+        }
+        if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent]=x_parent;
+        }else  if(rank[x_parent]<rank[y_parent]){
+            parent[x_parent]=y_parent;
+        }else{
+            parent[x_parent]=y_parent;
+            rank[y_parent]++;
+        }
+    }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int,vector<int>> adj;
-        if(source==destination) return true;
-        for(auto & edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        vector<bool> visited(n,false);
-        visited[source]=true;
-        queue<int> q;
-        q.push(source);
 
-        while(!q.empty()){
-            int N=q.size();
-            while(N--){
-                int node=q.front();
-                q.pop();
-                for(auto& v : adj[node]){
-                    if(!visited[v]){
-                        visited[v]=true;
-                        q.push(v);
-                        if(v==destination){
-                            return true;
-                        }
-                    }
-                }
-
-            }
+        //using DSU
+        parent.resize(n);
+        rank.resize(n,0);
+        for(int i=0 ; i<n ; i++){
+            parent[i]=i;
         }
-        return false;
+        for(auto &edge : edges){
+            Union(edge[0], edge[1]);
+        }
+        return find(source)==find(destination);
     }
 };

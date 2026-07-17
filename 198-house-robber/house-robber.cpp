cb8be13edc973159296b213161ca2bf1ab167f8d
pucchkii <147378380+pucchkii@users.noreply.github.com>
2026-07-17 17:48:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (78.89%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[101];
+
+    int solve(vector<int>& nums, int i, int n){
+        if(i>=n){
+            return 0;
+        }
+
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+
+        int steal = nums[i]+solve(nums,i+2,n);
+        int skip = solve(nums,i+1,n);
+
+        return dp[i]=max(steal,skip);
+
+
+    }
+    int rob(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        int n=nums.size();
+
+        return solve(nums,0,n);
+        
+    }
+};

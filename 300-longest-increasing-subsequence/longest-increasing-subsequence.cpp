09c3c5546028a662f93bc3228fd246b4dab525c0
pucchkii <147378380+pucchkii@users.noreply.github.com>
2026-07-18 12:32:02 +0530

    Time: 87 ms (44.13%) | Memory: 14.3 MB (71.09%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    //memoization
-    int t[2501][2501];
-    int n;
-    int solve(int idx, int prev, vector<int>& nums){
-        if(idx>=n) return 0;
-
-        if(prev!=-1 && t[idx][prev]!=-1){
-            return t[idx][prev];
-        }
-
-        //take
-        int take=0;
-        if(prev==-1 || nums[prev]<nums[idx]){
-            take=1+solve(idx+1,idx,nums);
-        }
-        //skip
-        int skip = solve(idx+1,prev,nums);
-
-        if(prev!=-1){
-            t[idx][prev]=max(skip,take);
-        }
-
-        return max(skip,take);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        n=nums.size();
-        memset(t,-1,sizeof(t));
+        int n=nums.size();
 
-        return solve(0,-1,nums);
+        vector<int> t(n+1,1);
+
+        int maxLIS = 1;
+
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<i ; j++){
+                if(nums[j]<nums[i]){
+                    t[i]=max(t[i],t[j]+1);
+                }
+                maxLIS = max(maxLIS, t[i]);
+            }
+        }
+        return maxLIS;
     }
 };

b1e37e4c40504a0d41409942846a564913f82b90
pucchkii <147378380+pucchkii@users.noreply.github.com>
2026-07-18 13:14:00 +0530

    Time: 76 ms (28.42%) | Memory: 27.3 MB (39.55%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-    int n;
-    int t[1001][1001];
+    int findLongestChain(vector<vector<int>>& nums) {
+        int n=nums.size();
 
-    int LIS(vector<vector<int>>& pairs , int idx, int prev){
-        if(idx==n){
-            return 0;
+        sort(begin(nums),end(nums));
+        vector<int> t(n+1,1);
+
+        int maxLIS=1;
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<i ; j++){
+                if(nums[j][1]<nums[i][0]){
+                    t[i]=max(t[i],t[j]+1);
+                }
+                maxLIS=max(maxLIS,t[i]);
+            }
         }
-
-        if(prev!=-1 && t[idx][prev]!=-1){
-            return t[idx][prev];
-        }
-
-        //take 
-        int take=0;
-        if(prev==-1 || pairs[idx][0]>pairs[prev][1]){
-            take=1+LIS(pairs,idx+1,idx);
-        }
-        //skip
-        int skip=LIS(pairs,idx+1,prev);
-
-        if(prev!=-1){
-            t[idx][prev]=max(skip,take);
-        }
-
-        return max(skip,take);
-    }
-
-    //using LIS
-
-    int findLongestChain(vector<vector<int>>& pairs) {
-        n=pairs.size();
-        memset(t,-1,sizeof(t));
-        sort(pairs.begin(),pairs.end());
-        return LIS(pairs,0,-1);
+        return maxLIS;
     }
 };

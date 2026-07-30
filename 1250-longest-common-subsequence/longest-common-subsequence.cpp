4fa0eae387a70edc6e62993d1b656621cf14e34e
Amit Yadav <147378380+pucchkii@users.noreply.github.com>
2026-07-30 11:58:38 +0530

    Time: 26 ms (65.03%) | Memory: 27.4 MB (55.27%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,28 +1,46 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.length();
-        int m=text2.length();
+    int longestCommonSubsequence(string s1, string s2) {
+        int n=s1.length();
+        int m=s2.length();
 
+        //creating vector dp[n+1][m+1];
         vector<vector<int>> t(n+1,vector<int>(m+1));
 
-        for(int row=0 ; row<n+1 ; row++){
-            t[row][0]=0;
+        // first row and first col is 0.
+        for(int i=0 ; i<n+1 ; i++){
+            t[i][0]=0;
         }
-        for(int col=0 ; col<m+1 ; col++){
-            t[0][col]=0;
+        for(int i=0 ; i<m+1 ; i++){
+            t[0][i]=0;
         }
-
-        for(int i=1 ; i<n+1; i++){
+        for(int i=1 ; i<n+1 ; i++){
             for(int j=1 ; j<m+1 ; j++){
-                if(text1[i-1]==text2[j-1]){
+                if(s1[i-1]==s2[j-1]){
                     t[i][j]=1+t[i-1][j-1];
                 }else{
                     t[i][j]=max(t[i-1][j],t[i][j-1]);
                 }
             }
         }
+        //created t array now finding lcs
+        string lcs="";
+        int i=n , j=m;
+        while(i>0 && j>0){
+            if(s1[i-1]==s2[j-1]){
+                lcs.push_back(s1[i-1]);
+                i--;
+                j--;
+            }else{
+                if(t[i-1][j]>t[i][j-1]){
+                    i--;
+                }else{
+                    j--;
+                }
+            }
+        }
+        reverse(begin(lcs),end(lcs));
+        cout<<lcs;
         return t[n][m];
-        
     }
 };

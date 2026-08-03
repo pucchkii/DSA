2e6ae3bd646f79ffbbafeae00114423a42a84065
Amit Yadav <147378380+pucchkii@users.noreply.github.com>
2026-08-03 18:19:49 +0530

    Time: 31 ms (26.80%) | Memory: 23.9 MB (76.10%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,53 +1,62 @@
 class Solution {
 public:
-
-    typedef pair<int,pair<int,int>> P;
-
-    vector<vector<int>> directions{{1,1},{0,1},{1,0},{-1,0},{0,-1},{-1,-1},{1,-1},{-1,1}};
-
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-
-        if(n==0 || m==0 || grid[0][0]!=0){
-            return -1;
+    bool isSafe(int x , int y , int m , int n){
+        if(x >= 0 && x < m && y >= 0 && y < n){
+            return true;
         }
 
-        auto isSafe=[&](int x, int y){
-            return x>=0 && x<n && y>=0 && y<m;
-        };
+        return false;
+    }
+    vector<vector<int>>directions{{1,1},{1,0},{0,1},{-1,-1},{-1,0},{0,-1},{-1,1},{1,-1}};
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        if(m == 0 || n == 0 || grid[0][0] == 1){
+            return -1;
+        }
+        
+        // pair<int , pair<int , int>>P;
 
-        //priority_queue<P,vector<P> , greater<P>> q;
-        queue<P> q;
-        vector<vector<int>> result(n,vector<int>(m,INT_MAX));
+        priority_queue<pair<int , pair<int , int>> , vector<pair<int , pair<int , int>>> , greater<pair<int , pair<int , int>>>>pq;
+        vector<vector<int>>result(m , vector<int>(n,INT_MAX));
+        
 
-        q.push({0,{0,0}});
-        result[0][0]=0;
+        result[0][0] = 0;
+        pq.push({0,{0,0}});
+        grid[0][0] = 1;
 
-        while(!q.empty()){
-            int d=q.front().first;
-            pair<int,int> node = q.front().second;
-            int x=node.first;
-            int y=node.second;
-            q.pop();
+        while(!pq.empty()){
+            int distance = pq.top().first;
+            auto coordinate = pq.top().second;
+            int x = coordinate.first;
+            int y = coordinate.second;
+            pq.pop();
+            if(distance > result[x][y]){
+                continue;
+            }
+            
 
-            for(auto & dir : directions){
+            for(auto &dir : directions){
                 int x_ = x + dir[0];
                 int y_ = y + dir[1];
-                int dist=1;
 
-                if(isSafe(x_ , y_) && grid[x_][y_]==0 && d+dist<result[x_][y_]){
-                    result[x_][y_]=d+dist;
-                    q.push({d+dist,{x_,y_}});
+                if(isSafe(x_,y_,m,n) && grid[x_][y_] != 1){
+                    if(result[x_][y_] > distance + 1){
+                        result[x_][y_] = distance + 1;
+                        pq.push({1+distance , {x_,y_}});
+                        grid[x_][y_] = 1;
+                    }
                 }
+                
             }
-        }
-        if(result[n-1][m-1]==INT_MAX){
-            return -1;
-        }else{
-            return result[n-1][m-1]+1;
+
+
         }
 
+        if(result[m-1][n-1] == INT_MAX){
+            return -1;
+        }
+        return result[m-1][n-1] + 1;
 
     }
 };

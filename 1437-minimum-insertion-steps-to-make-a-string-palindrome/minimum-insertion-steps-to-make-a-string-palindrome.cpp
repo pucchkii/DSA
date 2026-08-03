1503d5e7d924b87cf05694a234b63a99076f1640
Amit Yadav <147378380+pucchkii@users.noreply.github.com>
2026-08-04 03:49:52 +0530

    Time: 22 ms (80.28%) | Memory: 31.5 MB (39.11%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    int n;
-    int t[501][501];
-    int solve(string &s, int i, int j){
-        if(i>j) return 0;
-        if(t[i][j]!=-1){
-            return t[i][j];
-        }
-        if(s[i]==s[j]){
-            return t[i][j]=solve(s,i+1,j-1);
-        }else{
-            return t[i][j]=1+min(solve(s,i+1,j),solve(s,i,j-1));
-        }
-    }
     int minInsertions(string s) {
-        memset(t,-1,sizeof(t));
-        n=s.length();
-        return solve(s,0,n-1);
+        int n=s.length();
+
+        vector<vector<int>> t(n,vector<int>(n));
+        //state : t[i][j]---> t[i.......j] operations needed to make it palindorme
+        for(int i=0 ; i<n ; i++){
+            t[i][i]=0; //L=1 -->palindrome no operations needed;
+        }
+
+        for(int L=2 ; L<=n ; L++){
+            for(int i=0 ; i+L-1 <n ; i++){
+                int j=i+L-1;
+                if(s[i]==s[j]){
+                    t[i][j]=t[i+1][j-1];
+                }else {
+                    t[i][j]=1+min(t[i+1][j], t[i][j-1]);
+                }
+            }
+        }
+        return t[0][n-1];
+        
     }
 };

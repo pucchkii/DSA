fa07c829210eb571a791163b351412699a3b9a7a
Amit Yadav <147378380+pucchkii@users.noreply.github.com>
2026-08-04 16:04:31 +0530

    Time: 37 ms (55.47%) | Memory: 71.5 MB (41.75%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.cpp
+++ 922-possible-bipartition/possible-bipartition.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    bool dfs(unordered_map<int,vector<int>> &adj, int u, vector<int>& color, int& currColor){
-        color[u]=currColor;
-        for(auto& v : adj[u]){
-            if(color[v]==currColor){
-                return false;
-            }
-            if(color[v]==-1){
-                color[v]=1-currColor;
-                if(dfs(adj,v,color,color[v])==false){
+    bool bfs(unordered_map<int,vector<int>> &adj, int curr, vector<int>& color, int currColor){
+        color[curr]=currColor;
+        queue<int> q;
+        q.push(curr);
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            for(int &v : adj[u]){
+                if(color[v]==color[u]){
                     return false;
+                }else if(color[v]==-1){
+                    color[v]=1-color[u];
+                    q.push(v);
                 }
             }
         }
         return true;
+
+
     }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        // unordered map
         unordered_map<int,vector<int>> adj;
 
-        for(auto &e : dislikes){
+        for(auto& e : dislikes){
             int u=e[0];
             int v=e[1];
+
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-
         vector<int> color(n+1,-1);
         int currColor=1;
-        for(int i=1; i<=n ; i++){
+
+        for(int i=1 ; i<=n ; i++){
             if(color[i]==-1){
-                if(dfs(adj,i,color,currColor)==false){
+                if(bfs(adj,i,color,currColor)==false){
                     return false;
                 }
             }

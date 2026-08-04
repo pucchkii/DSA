14f65111dcafcd50e7173d906a6ec9de86764ddf
Amit Yadav <147378380+pucchkii@users.noreply.github.com>
2026-08-04 15:54:30 +0530

    Time: 29 ms (81.04%) | Memory: 71.3 MB (43.62%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.cpp
+++ 922-possible-bipartition/possible-bipartition.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-
-    bool dfs(unordered_map<int,vector<int>>& adj, int u, vector<int>& color, int currColor){
+    bool dfs(unordered_map<int,vector<int>> &adj, int u, vector<int>& color, int& currColor){
         color[u]=currColor;
-        for(auto & v : adj[u]){
+        for(auto& v : adj[u]){
             if(color[v]==currColor){
                 return false;
             }
@@ -17,18 +16,19 @@ public:
         return true;
     }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        vector<int> color(n+1,-1);
+        // unordered map
         unordered_map<int,vector<int>> adj;
 
-        for(auto &edge : dislikes){
-            int u=edge[0];
-            int v=edge[1];
+        for(auto &e : dislikes){
+            int u=e[0];
+            int v=e[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        int currColor=1;
 
-        for(int i=1 ; i<=n ; i++){
+        vector<int> color(n+1,-1);
+        int currColor=1;
+        for(int i=0 ; i<n ; i++){
             if(color[i]==-1){
                 if(dfs(adj,i,color,currColor)==false){
                     return false;

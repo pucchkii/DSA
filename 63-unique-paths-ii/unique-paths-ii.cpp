7d7ab5424a24a9767be24ab96ca1784ca507df54
Amit Yadav <147378380+pucchkii@users.noreply.github.com>
2026-08-05 15:00:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (96.83%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int n,m;
-        n=grid.size();
-        m=grid[0].size();
-
-        vector<vector<int>> t(n,vector<int>(m));
-        // state definition :
-        // t[i][j] --> indicates no of ways to reach (i,j) from (0,0);
-
-        t[0][0]=1; // distance form source to source is 0;
-        for(int i=0 ; i<m ; i++){
-            if(grid[0][i]==1){
-                break;
-            }else{
-                t[0][i]=1;
-            }
-        }
-        if(grid[n-1][m-1]==1){
+    // memoization
+    int t[101][101];
+    int n,m;
+    int solve(vector<vector<int>>& grid, int i, int j){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==1){
             return 0;
         }
-        for(int i=0 ; i<n ; i++){
-            if(grid[i][0]==1){
-                break;
-            }else{
-                t[i][0]=1;
-            }
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        if(t[i][j]!=-1){
+            return t[i][j];
         }
 
-        for(int i=1 ; i<n ; i++){
-            for(int j=1 ; j<m ; j++){
-                if(grid[i][j]==1){
-                    t[i][j]=0;
-                }else{
-                    t[i][j]=t[i-1][j]+t[i][j-1];
-                }
-            }
-        }
-       
-        return t[n-1][m-1];
+        int right=solve(grid,i+1,j);
+        int down=solve(grid,i,j+1);
+
+        return t[i][j]=right+down;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        m=grid.size();
+        n=grid[0].size();
+        memset(t,-1,sizeof(t));
+
+        return solve(grid,0,0);
     }
 };

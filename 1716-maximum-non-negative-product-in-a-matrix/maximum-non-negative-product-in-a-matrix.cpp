a768e4225f0be90a03d221eb49179eb9593ba304
Amit Yadav <147378380+pucchkii@users.noreply.github.com>
2026-08-05 17:17:57 +0530

    Time: 3 ms (31.43%) | Memory: 14.1 MB (72.51%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
@@ -4,42 +4,43 @@ public:
     typedef long long ll;
     int MOD =1e9+7;
 
-    // memoization
-    vector<vector<pair<ll,ll>>> t;
-
-    pair<ll,ll> solve(vector<vector<int>>& grid, int i, int j){
-        if(i==m-1 && j==n-1){
-            return {grid[i][j],grid[i][j]};
-        }
-        ll maxVal = LLONG_MIN;
-        ll minVal = LLONG_MAX;
-
-        if(t[i][j] != make_pair(LLONG_MIN, LLONG_MAX)){
-            return t[i][j];
-        }
-
-        // DOWN
-        if(i+1<m){
-            auto [downMax, downMin] = solve(grid, i+1,j);
-            maxVal = max ({maxVal , grid[i][j]*downMax, grid[i][j]*downMin});
-            minVal = min ({minVal , grid[i][j]*downMax, grid[i][j]*downMin});
-        }
-        // RIGHT
-        if(j+1<n){
-            auto [rightMax, rightMin] = solve(grid, i, j+1);
-            maxVal = max ({maxVal , grid[i][j]*rightMax, grid[i][j]*rightMin});
-            minVal = min ({minVal , grid[i][j]*rightMax, grid[i][j]*rightMin});
-        }
-        return t[i][j] = {maxVal,minVal};
-    }
     int maxProductPath(vector<vector<int>>& grid) {
         m=grid.size();
         n=grid[0].size();
 
-        // initializing t with LLONG_MIN and LLONG_MAX;
-        t = vector<vector<pair<ll,ll>>> (m,vector<pair<ll,ll>> (n, {LLONG_MIN, LLONG_MAX}));
-        auto [maxProd, minProd] = solve(grid, 0, 0);
+        // bottom up dp lena padega
+        vector<vector<pair<ll,ll>>> t(m,vector<pair<ll,ll>>(n));
+        // t[i][j] = (maxVal , minval) to reach form (0,0) to (i,j);
 
-        return maxProd < 0 ? -1 : maxProd % MOD; 
+        t[0][0] = make_pair(grid[0][0], grid[0][0]);
+
+        // filling the first row
+        for(int j=1 ; j<n ; j++){
+            t[0][j].first  = t[0][j-1].first * grid[0][j]; //maxVal
+            t[0][j].second = t[0][j-1].second * grid[0][j]; //minVal
+        }
+
+        // filling the first col
+        for(int i=1 ; i<m ; i++){
+            t[i][0].first  = t[i-1][0].first * grid[i][0]; //maxVal
+            t[i][0].second = t[i-1][0].second * grid[i][0]; //minVal
+        }
+        for(int i=1 ; i<m ; i++){
+            for(int j=1 ; j<n ; j++){
+                ll upMax = t[i-1][j].first;
+                ll upMin = t[i-1][j].second;
+
+                ll leftMax = t[i][j-1].first;
+                ll rightMin = t[i][j-1].second;
+
+                t[i][j].first = max({upMax * grid[i][j],upMin * grid[i][j],leftMax * grid[i][j],rightMin * grid[i][j]});
+
+                t[i][j].second = min({upMax * grid[i][j],upMin * grid[i][j],leftMax * grid[i][j],rightMin * grid[i][j]});
+
+            }
+        }
+        auto [maxProd, minProd] = t[m-1][n-1];
+        return maxProd < 0 ? -1 : maxProd % MOD;
     }
+
 };
